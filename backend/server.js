0d9cd54361b013c219c3eca896f0166cd9794496
alexandru-rx <165170964+alexandru-rx@users.noreply.github.com>
2026-04-22 10:16:24 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,18 @@ const eventRoutes = require("./routes/events");
 const paymentRoutes = require("./routes/payment");
 
 const app = express();
+app.set("trust proxy", 1);
 
 /****************************************************
  * FRONTEND ORIGINS
  * Allow local frontend pages to access backend API
  ****************************************************/
+
 const FRONTEND_ORIGINS = [
   "http://127.0.0.1:5500",
-  "http://localhost:5500"
-];
+  "http://localhost:5500",
+  process.env.FRONTEND_URL
+].filter(Boolean);
 
 /****************************************************
  * MIDDLEWARE: CORS
@@ -69,10 +72,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      sameSite: "lax",
-      secure: false // local development over http
-    }
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production"
+}
   })
 );
 
